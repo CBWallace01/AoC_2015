0dc91f3db158e9f426e27ce76e4635f59132cd75
Chris Wallace <cbwallace01@gmail.com>
2019-12-07 21:41:01 -0600

    Day 17 started...

---
+++ Day17.py
@@ -0,0 +1,18 @@
+from PuzzleInput import ReadInput
+
+pz_input = ReadInput(17).data
+
+
+def part_a():
+    floor = 0
+    return floor
+
+
+def part_b():
+    floor = 0
+    return floor
+
+
+if __name__ == "__main__":
+    print("Part A", part_a())
+    print("Part B", part_b())
